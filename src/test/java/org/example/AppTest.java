44f9edd6d9a630f0cf01ada7eebd6c617840cdaf
Dominik Mehlich <dominik.v.mehlich@fau.de>
2022-07-20 21:52:54 +0200

    Initial commit

---
+++ src/test/java/org/example/AppTest.java
@@ -0,0 +1,15 @@
+package org.example;
+
+import org.junit.jupiter.api.*;
+
+
+public class AppTest {
+
+    @Test
+    public void shouldAnswerTrue() {
+        Assertions.assertTrue(true);
+    }
+
+
+
+}
